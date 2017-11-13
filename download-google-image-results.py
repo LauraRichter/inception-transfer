2facf1860de3862704540e8c2e313e1827cfde92
LauraRichter <llrichter@gmail.com>
2017-11-13 16:43:09 +0200

    Keep track of number downloaded; Output error if some don't download; Resize if necessary.

--- download-google-image-results.py
+++ download-google-image-results.py
@@ -15,10 +15,24 @@ def download_images(image_search_query, number):
     im_dir = Path('images', image_search_query.replace(' ', '_'))
     im_dir.mkdir(exist_ok=True, parents=True)
     # loop through images and save
-    for ii, im in enumerate(images):
-        im_format = im.format
-        im_path = im_dir / '{}.{}'.format(ii, im_format)
-        im.to_file(str(im_path))
+    n_downloaded = 0
+    for im in images:
+        try:
+            im_format = im.format
+            im_path = str(im_dir / '{}.{}'.format(n_downloaded, im_format))
+            # resize, if necessary, then save
+            xsize, ysize = im.size
+            max_size = 200
+            if ysize > max_size:
+                im_to_save = im.reduce(new_height=max_size)
+                im_to_save.save(im_path)
+            else:
+                with open(im_path, 'wb') as f:
+                    f.write(im.content.getvalue())
+            n_downloaded += 1
+        except Exception as err:
+            print('Failed download of image {}: {}'.format(im.URL, err))
+    print('Number of successful downloads: {}'.format(n_downloaded))
 
 
 if __name__ == '__main__':
